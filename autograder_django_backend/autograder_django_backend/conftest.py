88511eb603a983a3bdb6caebbc7c9ce3f3f6fd40
Forest Anderson <forestkzanderson@gmail.com>
2022-03-31 00:33:51 +0000

    Initial Django cookiecutter

---
+++ autograder_django_backend/autograder_django_backend/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from autograder_django_backend.users.models import User
+from autograder_django_backend.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
