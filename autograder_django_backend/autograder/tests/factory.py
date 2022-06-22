1a18a4b08e61edb893ff48a7df2e0c4eda1acff1
Nguyen-HanhNong <nongnguyenhanh@gmail.com>
2022-06-22 22:06:17 +0000

    course and professor factory code so far

--- autograder_django_backend/autograder/tests/factory.py
+++ autograder_django_backend/autograder/tests/factory.py
@@ -5,8 +5,6 @@ from autograder.models import Student
 fake = Faker()
 
 # Create a student
-
-
 def student_factory(**kwargs):
     if "name" not in kwargs:
         kwargs["name"] = fake.name()
@@ -19,3 +17,37 @@ def student_factory(**kwargs):
     student = Student.objects.create(**kwargs)
 
     return student
+
+#Create a professor
+def professor_factory(**kwargs):
+    if "name" not in kwargs:
+        kwargs["name"] = fake.name()
+    if "email" not in kwargs:
+        kwargs["email"] = fake.email()
+
+    # Check that there are only 2 arguments in kwargs
+    assert len(kwargs) == 2
+
+    professor = Professor.objects.create(**kwargs)
+
+    return professor
+
+#Create a course
+def course_factory(**kwargs):
+    if "course_id" not in kwargs:
+        kwargs["course_id"] = fake.course_id()
+    if "name" not in kwargs:
+        kwargs["name"] = fake.name()
+    if "description" not in kwargs:
+        kwargs["description"] = fake.description()
+    if "section" not in kwargs:
+        kwargs["section"] = fake.section()
+    if "professor" not in kwargs: 
+        kwargs["professor"] = professor_factory();
+
+    # Check that there are only 5 arguments in kwargs
+    assert len(kwargs) == 5
+
+    course = Course.objects.create(**kwargs)
+
+    return course
